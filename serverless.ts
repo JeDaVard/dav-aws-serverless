d908361eeddcef3bf44c81e6d354d7ed33ffeb75
Davit Vardanyan <jedavard@gmail.com>
2020-11-08 21:55:35 +0400

    init commit

---
+++ serverless.ts
@@ -0,0 +1,42 @@
+import type { Serverless } from 'serverless/aws';
+
+
+const serverlessConfiguration: Serverless = {
+  service: 'dav-aws-serverless',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      // @ts-ignore https://www.serverless.com/framework/docs/deprecations/#AWS_API_GATEWAY_NAME_STARTING_WITH_SERVICE
+      shouldStartNameWithService: true
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+  },
+  functions: {
+    hello: {
+      handler: 'handler.hello',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'hello',
+          }
+        }
+      ]
+    }
+  }
+}
+
+module.exports = serverlessConfiguration;
